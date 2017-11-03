756edef6f65f1a745f47a569f427e7a5e8fc7587
cheetah <inchaiya.keta@gmail.com>
2017-11-03 19:15:08 +0000

    Sending and receiving file are successful.

--- src/Client.java
+++ src/Client.java
@@ -3,81 +3,107 @@
 import java.io.*;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 
 public class Client {
 
-	private Socket socket;
-	private String host;
-	private int port;
-	
-	private BufferedReader br;
-	private PrintWriter pw;
-	
-	private InputStream test_is;
+    private Socket socket;
+    private String host;
+    private int port;
 
-	public Client(String host, int port){
-		this.host = host;
-		this.port = port;
-		runClient();
-	}
+    private BufferedReader br;
+    private PrintWriter pw;
 
-	
-	private void runClient(){
-		try {
-			this.socket = new Socket(host, port);
-			System.out.println("Client connected to " + host + " on port " + port + ".");
-			System.out.println("To exit enter a single line containing: " + Configuration.exitString);
-			br = new BufferedReader(new InputStreamReader(System.in));
-			pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-			test_is = socket.getInputStream();
-			printUserInputToSocket(); // this runs until something goes wrong
-			
-		} catch (Exception e){ // exit cleanly for any Exception (including IOException, SocketTimeoutException, DisconnectedException) 
-			System.out.println("Ooops on connection to " + host + " on port " + port + ". " + e.getMessage());
-			cleanup(); // execute cleanup method to close connections cleanly
-		} 
-	}
-	
-	// test to see if connection to server is still ok by trying to 
-	// read the ACK byte the server returns to the client
-	// set a timeout for the blocking read in case the server has died 
-	// or the connection has otherwise been broken. If no result is read 
-	// within the specified timeout the read method throws SocketTimeoutException
-	// which will be passed up the call-stack to the nearest handler (catch block)
-	// in the runClient method. If -1 is read, a DisconnectedException is thrown.
-	private void testServerConnection() throws IOException, DisconnectedException, SocketTimeoutException {
-		int old_timeout = socket.getSoTimeout();
-		socket.setSoTimeout(500);
-		int res = test_is.read(); // this will throw an SocketTimeoutException if the connection to the server has been reset or times out
-		if(res == -1){
-			throw new DisconnectedException("... connection has been lost ...");
-		}
-		socket.setSoTimeout(old_timeout);
-	}
-	
-	private void printUserInputToSocket() throws IOException, SocketTimeoutException, DisconnectedException {
-		while(true){
-			String line = br.readLine(); // get user input
-			pw.println(line);            // print line out on the socket's output stream
-			pw.flush();                  // flush the output stream so that the server gets message immediately			
-			if(line.equals(Configuration.exitString)){ // user has entered exit command
-				throw new DisconnectedException(" ... user has entered exit command ... ");
-			}
-			testServerConnection(); // test to see if connection to server is still ok by trying to read the ACK byte the server returns to the client
-		}
-	} 
-	
-	
-	private void cleanup(){
-		System.out.println("Client: ... cleaning up and exiting ... " );
-		try {
-			if(pw != null) pw.close();
-			if(br != null) br.close();
-			if(socket != null) socket.close();
-		} catch (IOException ioe){
-			System.out.println("Ooops " + ioe.getMessage());			
-		}
-	}
-		
+    private InputStream test_is;
+
+    public Client(String host, int port) {
+        this.host = host;
+        this.port = port;
+        runClient();
+    }
+
+
+    private void runClient() {
+        try {
+            this.socket = new Socket(host, port);
+            System.out.println("Client connected to " + host + " on port " + port + ".");
+            System.out.println("To exit enter a single line containing: " + Configuration.exitString);
+            br = new BufferedReader(new InputStreamReader(System.in));
+            pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+            test_is = socket.getInputStream();
+            printUserInputToSocket(); // this runs until something goes wrong
+
+        } catch (Exception e) { // exit cleanly for any Exception (including IOException, SocketTimeoutException, DisconnectedException)
+            System.out.println("Ooops on connection to " + host + " on port " + port + ". " + e.getMessage());
+            cleanup(); // execute cleanup method to close connections cleanly
+        }
+    }
+
+    // test to see if connection to server is still ok by trying to
+    // read the ACK byte the server returns to the client
+    // set a timeout for the blocking read in case the server has died
+    // or the connection has otherwise been broken. If no result is read
+    // within the specified timeout the read method throws SocketTimeoutException
+    // which will be passed up the call-stack to the nearest handler (catch block)
+    // in the runClient method. If -1 is read, a DisconnectedException is thrown.
+    private void testServerConnection() throws IOException, DisconnectedException, SocketTimeoutException {
+        int old_timeout = socket.getSoTimeout();
+        socket.setSoTimeout(500);
+        int res = test_is.read(); // this will throw an SocketTimeoutException if the connection to the server has been reset or times out
+        if (res == -1) {
+            throw new DisconnectedException("... connection has been lost ...");
+        }
+        socket.setSoTimeout(old_timeout);
+    }
+
+    public void receiveResource() {
+        try {
+            InputStream in = socket.getInputStream();
+            OutputStream out = new FileOutputStream("/Users/cheetah/Sites/CS5001-p3-networking/Resources/beer.jpg");
+            copy(in, out);
+            out.close();
+            in.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void copy(InputStream in, OutputStream out) throws IOException {
+        byte[] buf = new byte[8192];
+        int len = 0;
+        while ((len = in.read(buf)) != -1) {
+            out.write(buf, 0, len);
+        }
+    }
+
+    private void printUserInputToSocket() throws IOException, SocketTimeoutException, DisconnectedException {
+        while (true) {
+            String line = br.readLine(); // get user input
+            pw.println(line);            // print line out on the socket's output stream
+            if (line.equals(Configuration.exitString)) { // user has entered exit command
+                throw new DisconnectedException(" ... user has entered exit command ... ");
+            } else if (line.equalsIgnoreCase("keta")) {
+                receiveResource();
+            }
+            testServerConnection(); // test to see if connection to server is still ok by trying to read the ACK byte the server returns to the client
+        }
+    }
+
+
+    private void cleanup() {
+        System.out.println("Client: ... cleaning up and exiting ... ");
+        try {
+            if (pw != null) pw.close();
+            if (br != null) br.close();
+            if (socket != null) socket.close();
+        } catch (IOException ioe) {
+            System.out.println("Ooops " + ioe.getMessage());
+        }
+    }
+
 }
 
--- src/ConnectionHandler.java
+++ src/ConnectionHandler.java
@@ -5,62 +5,84 @@ import java.net.Socket;
 
 public class ConnectionHandler extends Thread {
 
-	private Socket conn;       // socket representing TCP/IP connection to Client
-	private InputStream is;    // get data from client on this input stream	
-	private OutputStream os;   // can send data back to the client on this output stream
-	BufferedReader br;         // use buffered reader to read client data
+    private Socket conn;       // socket representing TCP/IP connection to Client
+    private InputStream is;    // get data from client on this input stream
+    private OutputStream os;   // can send data back to the client on this output stream
+    BufferedReader br;         // use buffered reader to read client data
 
-	public ConnectionHandler(Socket conn) {
-		this.conn = conn;
-		try{
-			is = conn.getInputStream();     // get data from client on this input stream
-			os = conn.getOutputStream();  // to send data back to the client on this stream
-			br = new BufferedReader(new InputStreamReader(is)); // use buffered reader to read client data
-		} catch (IOException ioe){
-			System.out.println("ConnectionHandler: " + ioe.getMessage());
-		}
-	}
+    public ConnectionHandler(Socket conn) {
+        this.conn = conn;
+        try {
+            is = conn.getInputStream();     // get data from client on this input stream
+            os = conn.getOutputStream();  // to send data back to the client on this stream
+            br = new BufferedReader(new InputStreamReader(is)); // use buffered reader to read client data
+        } catch (IOException ioe) {
+            System.out.println("ConnectionHandler: " + ioe.getMessage());
+        }
+    }
 
-	public void run() { // run method is invoked when the Thread's start method (ch.start(); in Server class) is invoked  
-		System.out.println("new ConnectionHandler thread started .... ");
-		try {
-			printClientData();
-		} catch  (Exception e){ // exit cleanly for any Exception (including IOException, ClientDisconnectedException)
-			System.out.println("ConnectionHandler:run " + e.getMessage());
-			cleanup();     // cleanup and exit
-		}
-	}
+    public void run() { // run method is invoked when the Thread's start method (ch.start(); in Server class) is invoked
+        System.out.println("new ConnectionHandler thread started .... ");
+        try {
+            printClientData();
+        } catch (Exception e) { // exit cleanly for any Exception (including IOException, ClientDisconnectedException)
+            System.out.println("ConnectionHandler:run " + e.getMessage());
+            cleanup();     // cleanup and exit
+        }
+    }
 
+    public void sendResource() {
+        try {
+            InputStream in = new FileInputStream("/Users/cheetah/Sites/CS5001-p3-networking/Resources/www/beer.jpg");
+            OutputStream out = conn.getOutputStream();
+            copy(in, out);
+            out.close();
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private void printClientData() throws DisconnectedException, IOException {
-		while(true) {
-			String line = br.readLine(); // get data from client over socket
-			// if readLine fails we can deduce here that the connection to the client is broken
-			// and shut down the connection on this side cleanly by throwing a DisconnectedException
-			// which will be passed up the call stack to the nearest handler (catch block)
-			// in the run method
-			if(line == null || line.equals("null") || line.equals(Configuration.exitString) ){
-				throw new DisconnectedException(" ... client has closed the connection ... ");
-			} 
-			// in this simple setup all the server does in response to messages from the client is to send
-			// a single ACK byte back to client - the client uses this ACK byte to test whether the 
-			// connection to this server is still live, if not the client shuts down cleanly
-			os.write(Configuration.ackByte);  
-			System.out.println("ConnectionHandler: " + line); // assuming no exception, print out line received from client
-		}
-	}
+    public void copy(InputStream in, OutputStream out) throws IOException {
+        byte[] buf = new byte[8192];
+        int len = 0;
+        while ((len = in.read(buf)) != -1) {
+            out.write(buf, 0, len);
+        }
+    }
 
-	private void cleanup(){
-		System.out.println("ConnectionHandler: ... cleaning up and exiting ... " );
-		try{
-			br.close();
-			is.close();
-			conn.close();
-		} catch (IOException ioe){
-			System.out.println("ConnectionHandler:cleanup " + ioe.getMessage());
-		}
-	}
+    private void printClientData() throws DisconnectedException, IOException {
+        while (true) {
+            String line = br.readLine(); // get data from client over socket
+            // if readLine fails we can deduce here that the connection to the client is broken
+            // and shut down the connection on this side cleanly by throwing a DisconnectedException
+            // which will be passed up the call stack to the nearest handler (catch block)
+            // in the run method
+            if (line == null || line.equals("null") || line.equals(Configuration.exitString)) {
+                throw new DisconnectedException(" ... client has closed the connection ... ");
+            } else if (line.equalsIgnoreCase("keta")) {
+                sendResource();
+            }
+            // in this simple setup all the server does in response to messages from the client is to send
+            // a single ACK byte back to client - the client uses this ACK byte to test whether the
+            // connection to this server is still live, if not the client shuts down cleanly
+            os.write(Configuration.ackByte);
+            System.out.println("ConnectionHandler: " + line); // assuming no exception, print out line received from client
+        }
+    }
 
+    private void cleanup() {
+        System.out.println("ConnectionHandler: ... cleaning up and exiting ... ");
+        try {
+            br.close();
+            is.close();
+            conn.close();
+        } catch (IOException ioe) {
+            System.out.println("ConnectionHandler:cleanup " + ioe.getMessage());
+        }
+    }
 
 
 }
